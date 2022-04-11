1c1e6947e8fca08b5c5cc41bdae98c7716850290
GONZA123A <gonzalosarabia79@gmail.com>
2022-04-11 10:33:46 -0300

    porque quiero ser mi primer proyecto

---
+++ src/app/pages/exitos/exitos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exitos',
+  templateUrl: './exitos.component.html',
+  styleUrls: ['./exitos.component.css']
+})
+export class ExitosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
